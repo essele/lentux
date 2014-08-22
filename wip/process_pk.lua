d9ae9ab93c1fb2e4ba23f5305640769e99bc8c0e
Lee Essen <lee.essen@nowonline.co.uk>
2014-08-22 07:01:27 +0100

    better templating support

--- wip/process_pk.lua
+++ wip/process_pk.lua
@@ -10,18 +10,35 @@
 --
 
 local required_fields = {
-	"name", "url", "version", "source-archive"
+	"name", "source-url", "version"
 }
 
 local string_fields = {
-	"name", "url", "version", "source-archive"
+	"name", "version", "source-archive", "source-dir",
+	"NAME"
 }
 
-local template = {
-	["install-dir"] =	{ "$(BASE_DIR)/output/target" },
-	["configure"] = 	{ "configure --prefix=/usr" },
-	["make"] = 			{ "make" },
-	["install"] = 		{ "make DESTDIR=$[install-dir]" }
+local pre_expand_template = {
+	["install-dir"] =		{ "$(BASE_DIR)/output/target" },
+	["install-prefix"] =	{ "/usr" },
+	["configure"] = 		{ "configure --prefix=/usr" },
+	["make"] = 				{ "make" },
+	["install"] = 			{ "make DESTDIR=$[install-dir]" },
+	["source-archive"] =	{ "$[name]-$[version].tar.gz" },
+	["source-dir"] =		{ "$[name]-$[version]" },
+	["template"] =			{ "autotools" },
+	["template-file"] = 	{ "$[template].tmpl" }
+}
+
+local unpackers = {
+	[".tar.gz"] = "tar -C $(BUILD_DIR) -zxf $<",
+	[".tgz"] = "tar -C $(BUILD_DIR) -zxf $<",
+}
+
+local post_expand_template = {
+	["NAME"] = function(v) return { v["name"][1]:upper() } end,
+	["FRED"] = function(v) return { "this should be fred" } end,
+	["unpack-cmd"] = function(v) return { find_unpacker(v["source-archive"][1]) } end
 }
 
 --
@@ -30,6 +47,16 @@ local template = {
 function usage()
 	print("Usage: " .. arg[0] .. " <package_file>")
 	os.exit(1)
+end
+
+--
+-- Find an unpack command based on the file extension
+--
+function find_unpacker(filename)
+	for k,v in pairs(unpackers) do
+		if(filename:sub(-#k) == k) then return v end
+	end
+	return "$(error no unpacker known for " .. filename .. ")"
 end
 
 --
@@ -76,7 +103,6 @@ function read_pk(filename)
 			curkey = k
 			if(not rc[curkey]) then rc[curkey] = {} end
 			table.insert(rc[curkey], v)
-			print(string.format("%s: %s", curkey, v))
 			goto continue
 		end
 
@@ -84,23 +110,36 @@ function read_pk(filename)
 		v = line:match("^%s+([^%s].*)$")
 		if(v) then
 			if(not curkey) then
-				err = string.format("Illegal continuation at line %d, ignoring", ln)
+				err = string.format("Illegal continuation at line %d", ln)
 				break;
 			end
 			table.insert(rc[curkey], v)
-			print(string.format("%s: %s", curkey, v))
+			goto continue
+		end
+
+		-- if we didn't start with an indent, then we clear curkey
+		curkey = nil
+
+		-- handle <include>
+		if(line:match("^<include>")) then
+			rc["include"] = {}
+			while(true) do
+				line = f:read()
+				if(not line) then break end
+				table.insert(rc["include"], line)
+			end
 			goto continue
 		end
 
 		-- just blanks --
-		if(line:match("^%s*$")) then
-			curkey = nil
-			goto continue
-		end
+		if(line:match("^%s*$")) then goto continue end
+
+		-- comments
+		if(line:match("^#")) then goto continue end
 
 		-- must be an error --
 		if(true) then
-			err = string.format("garbage at line %d, ignoring", ln)
+			err = string.format("garbage at line %d", ln)
 			break
 		end
 
@@ -113,14 +152,33 @@ function read_pk(filename)
 end
 
 --
--- Expand any variables for the given key, note that the order the
--- expansion is done is relevant, there is no recursion
+-- Simple return an iterator of all variables referenced by the given
+-- key, this is so that we can check for dependencies and do them
+-- in the right order
 --
-function var_expand(vars, key)
-	local items = vars[key]
+function referenced_variables(vars, key)
+	local rc = {}
+	local last
+
+	for i, v in ipairs(vars[key]) do
+		for var in v:gmatch("[@$]%[([^]]+)%]") do
+			rc[var] = 1
+		end
+	end
+	return function()
+		last = next(rc, last)
+		return last
+	end
+end
+
+--
+-- Expand any variables for the given key (we assume dependencies have
+-- been handled before calling)
+--
+function expand_variable(vars, key)
 	local new = {}
 
-	for i, v in ipairs(items) do
+	for i, v in ipairs(vars[key]) do
 		local newitem
 
 		-- expansion using $[] expands to space separated (single line)
@@ -143,32 +201,75 @@ function var_expand(vars, key)
 end
 
 --
+-- Loop through expanding all the variables, we need to keep going until
+-- we have done no work
+--
+function expand_all_variables(vars)
+	local done = {}
+
+	repeat 	
+		local processed = 0
+		for k, v in pairs(vars) do
+			if(done[k]) then goto continue end
+
+			-- see if all our referenced variables have been done, if not
+			-- we have to skip this one for now
+			for r in referenced_variables(vars, k) do
+				if(not done[r]) then goto continue end
+			end
+
+			-- ok, we can expand
+			expand_variable(vars, k)
+			done[k] = 1
+			processed = 1
+::continue::
+		end
+	until(processed == 0)
+end
+
+
+--
 -- For items that are supposed to be single (i.e. not lists) then
 -- we can check, and if valid we can change over to a string.
 --
-function validate_strings(vars, list)
-	local err = 0
-
+function strings_are_ok(vars, list)
 	for _, i in ipairs(list) do
 		if(vars[i]) then
 			if(#vars[i] ~= 1) then
-				print("Expect only one item for: " .. i)
-				err = 1
+				print("Multiple items for: " .. i)
+				return false
 			else
 				vars[i] = vars[i][1]
 			end
 		end
 	end
-	return err
+	return true
 end
 
 --
--- Add any items that are in our template that we haven't overridden
+-- Check for required fields
 --
-function add_template(vars, template)
-	for k,v in pairs(template) do
+function all_required_ok(vars, reqd)
+	for _,i in ipairs(reqd) do
+		if(not vars[i]) then
+			print("Missing variable: " .. i)
+			return false
+		end
+	end
+	return true
+end
+
+--
+-- Add any non-defined variables (inc. function calls)
+--
+function add_variables(vars, actions)
+	for k,v in pairs(actions) do
 		if(not vars[k]) then
-			vars[k] = table_copy(template[k])
+			if(type(v) == "function") then
+				vars[k] = v(vars)
+			else
+				vars[k] = table_copy(v)
+			end
 		end
 	end
 end
@@ -192,9 +293,24 @@ function dump(vars)
 	end
 end
 
+--
+-- Read a file into a given field
+--
+function read_file(filename, vars, key)
+	local data = {}
+	local file = io.open(filename)
 
+	if(not file) then
+		print("Unable to open file: " .. filename)
+		return false
+	end
 
-
+	for l in file:lines() do
+		table.insert(data, l)
+	end
+	vars[key] = data
+	return true
+end
 
 
 if(not arg[1]) then usage() end
@@ -209,26 +325,41 @@ if(not pkg) then
 end
 
 --
--- Add in the template variables
+-- Add in the template variables in the pre-expand stage
 --
-add_template(pkg, template)
+add_variables(pkg, pre_expand_template)
 
 --
--- Make sure we now have all required fields
+-- Now we can expand any embedded variables
 --
--- TODO
+expand_all_variables(pkg)
+
+--
+-- Add in the template variables for the post-expand stage
+--
+add_variables(pkg, post_expand_template)
+
+--
+-- Read in our template file
+--
+if(not read_file(pkg["template-file"][1], pkg, ".template")) then os.exit(1) end
+
+--
+-- Expand any variables in the template
+--
+expand_variable(pkg, ".template")
 
 
-print("source-archive=" .. table.concat(pkg["source-archive"], "\n"))
 
-var_expand(pkg, "source-archive")
-var_expand(pkg, "fred")
-print("--")
+--
+-- Make sure we now have all required variables and strings are ok
+--
+if(not all_required_ok(pkg, required_fields)) then os.exit(1) end
+if(not strings_are_ok(pkg, string_fields)) then os.exit(1) end
 
-print("fred=" .. table.concat(pkg.fred, "\n"))
-
-validate_strings(pkg, { "source-archive", "name", "version" })
-
-print("source-archive=" .. pkg["source-archive"])
-
+--
+--
+--
 dump(pkg)
+
+
